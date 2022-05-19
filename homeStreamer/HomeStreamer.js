322e61cfac30eb860be7d4f8d5967ea77afd0215
JuanBenitez2600 <juan_manuel.benitez@uao.edu.co>
2022-05-19 18:13:59 -0500

    Cambios -Bet

---
+++ homeStreamer/HomeStreamer.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import "./HomeStreamer.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navar from '../../components/nav/Navar'
+import ComprobarLogin from '../../components/comprobarLogin/ComprobarLogin'
+
+function HomeStreamer() {
+
+return(
+    <div className="HomeStreamer">
+    <ComprobarLogin />
+    <Navar/>
+
+
+    <h2 class="h2pru">Eventos</h2>
+    <div class="contenedor">
+      <table class="table table-light">
+        <thead>
+          <tr>
+            <th scope="col">Numero</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Fecha</th>
+            <th scope="col">Cantidad de participantes</th>
+            <th scope="col">Imagen</th>
+            <th scope="col">Estado</th>
+          </tr>
+        </thead>
+        <tbody id="personajes">
+        </tbody>
+      </table>
+
+      <a class="centrar" href="./streamer/eventos"><button class=" btn btn-primary btn-lg botonlarge centrar" onclick="">Administrar Eventos</button></a>
+    </div>
+
+
+
+    </div>
+)
+}
+export default HomeStreamer;
